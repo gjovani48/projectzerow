ec54142f50ed0adb6fbe7b5411148fcdc8796837
gjovani48 <gjovani1997@gmail.com>
2020-02-27 20:56:16 +0800

    Yesy

--- model/cart.js
+++ model/cart.js
@@ -5,22 +5,31 @@ const Schema = mongoose.Schema;
 mongoose.connect('mongodb+srv://projectzerow:projectzerow@cluster0-hux7n.mongodb.net/projectzerow?retryWrites=true&w=majority',{useNewUrlParser:true, useUnifiedTopology: true})
 
 
-const Cart = mongoose.model('cart', {
-
+var product_schema = new Schema({
     product_id:{
         type: Schema.Types.ObjectId,
         ref:'product',
-        require: true
+        require: false
     },
+    quantity: {
+      type: Number,
+      require: false
+    }
+});
 
+const Cart = mongoose.model('cart', {
+
+    product:[
+      product_schema
+    ],
     user_id:{
         type: Schema.Types.ObjectId,
         ref:'user',
         require: true
     },
-	items_count: {
-        type: Number,
-        require: true
+	items_count:{
+      type:Number,
+      require: false,
     },
     created_date:{
         type: Date,
@@ -30,3 +39,4 @@ const Cart = mongoose.model('cart', {
 })
 
 module.exports = Cart
+
--- routes/cart.js
+++ routes/cart.js
@@ -4,12 +4,11 @@ const Cart = require('../model/cart')
 const bodyParser = require('body-parser')
 const urlEncoded = bodyParser.json()
 
-// Get All Carts
 router.get('/', (req,res) => {
-    Cart.find({}).populate(['product_id','user_id']).exec((err, data) => {
-        if(err) throw err
-        res.json(data)
-    })
+  Cart.find({}).populate(['product_id','user_id']).exec((err, data) => {
+      if(err) throw err
+      res.json(data)
+  })
 })
 
 // router.get('/',(req,res)=>{
@@ -21,25 +20,44 @@ router.get('/', (req,res) => {
 
 // Get Cart by ID
 router.get('/:id', (req,res) => {
-    Cart.findOne({user_id:req.params.id}).populate(['product_id','user_id']).exec((err, data) => {
-        if(err) throw err
-        res.json(data)
-    })
+  Cart.findOne({user_id:req.params.id}).populate(['user_id']).exec((err, data) => {
+      if(err) throw err
+      res.json(data)
+  })
 
 })
 
 // Add New Cart
 router.post('/', urlEncoded,(req,res) => {
-    var cart = new Cart({
-      product_id: req.body.product_id,
-      user_id: req.body.user_id
-    })
+  var cart = new Cart({
+    product_id: req.body.product_id,
+    user_id: req.body.user_id
+  })
 
-    cart.save( (err) => {
-        if(err) res.json({msg:"Invalid Request"})
-        res.json({msg:"Product Added"})
-    })
+  cart.save( (err) => {
+      if(err) res.json({msg:"Invalid Request"})
+      res.json({msg:"Product Added"})
+  })
 })
+
+//addtocart
+router.post('/addtocart', urlEncoded, (req,res) => {
+  Cart.updateOne({ user_id: req.body.user_id},
+    {
+      $push: { product: {product_id:req.body.product_id, quantity: req.body.quantity} }
+    }
+    ,(err) => {
+      if(err) res.json({msg:err})
+
+      Cart.updateOne({ user_id: req.body.user_id},{$inc:{items_count:1}},(err)=>{
+
+        if(err) res.json({msg:err})
+        res.json([{msg:req.body.user_id}])
+
+      })
+  })
+})
+
 
 // Update Cart by ID
 router.put('/:id', urlEncoded, (req,res) => {
