035e540f6b257e39331464b10ffa3dec8729f72c
gjovani48 <gjovani1997@gmail.com>
2020-03-08 15:05:11 +0800

    Late commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { UserService, UserDetails} from './services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'projectzerow';
+  details: UserDetails;
+
+  constructor(private userService: UserService,private router: Router) { }
+
+  ngOnInit() {
+    this.getProfile();
+  }
+
+  getProfile(){
+    this.userService.profile().subscribe(
+      user=>{
+        this.details = user;
+
+        console.log(this.details);
+      },
+      err=>{
+        console.log(err);
+        this.router.navigateByUrl('/login');
+      }
+    )
+
+
 }
+
+}
+
+
+
